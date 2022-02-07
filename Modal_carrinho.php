5188deac7bf6cbdc72bdb867280280a0a757cd5c
anderson-tec445 <andersondepaula2304@gmail.com>
2022-02-07 16:03:44 -0300

    modal carrinho

--- Modal_carrinho.php
+++ Modal_carrinho.php
@@ -69,6 +69,7 @@
 
                 if (mensagem == 'Cadastrado com Sucesso!!') {
                     atualizarCarrinho();
+                    toggleCarrinho();
                     $("#modalCarrinho").modal("show");
                     //$('#mensagem').text(mensagem);
                 } else {
