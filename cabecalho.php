2af76e2e51f34c67e9f2c5010c288900b28d0758
Thiago de Paula <thiago_newdesign@hotmail.com>
2022-02-19 12:43:11 -0300

    teste face

--- cabecalho.php
+++ cabecalho.php
@@ -95,32 +95,91 @@ for ($i = 0; $i < count($dados); $i++) {
 
 <!-- Login Facebook -->
 <script>
+
+  function statusChangeCallback(response) {  // Called with the results from FB.getLoginStatus().
+    console.log('statusChangeCallback');
+    console.log(response);                   // The current login status of the person.
+    if (response.status === 'connected') {   // Logged into your webpage and Facebook.
+      testAPI();  
+    } else {                                 // Not logged into your webpage or we are unable to tell.
+      document.getElementById('status').innerHTML = 'Please log ' +
+        'into this webpage.';
+    }
+  }
+
+
+  function checkLoginState() {               // Called when a person is finished with the Login Button.
+    FB.getLoginStatus(function(response) {   // See the onlogin handler
+      statusChangeCallback(response);
+    });
+  }
+
+
   window.fbAsyncInit = function() {
     FB.init({
       appId      : '451774609941079',
-      cookie     : true,
-      xfbml      : true,
-      version    : 'v13.0'
+      cookie     : true,                     // Enable cookies to allow the server to access the session.
+      xfbml      : true,                     // Parse social plugins on this webpage.
+      version    : 'v13.0'           // Use this Graph API version for this call.
     });
-      
-    FB.AppEvents.logPageView();   
-      
+
+
+    FB.getLoginStatus(function(response) {   // Called after the JS SDK has been initialized.
+      statusChangeCallback(response);        // Returns the login status.
+    });
   };
-
-  (function(d, s, id){
-     var js, fjs = d.getElementsByTagName(s)[0];
-     if (d.getElementById(id)) {return;}
-     js = d.createElement(s); js.id = id;
-     js.src = "https://connect.facebook.net/en_US/sdk.js";
-     fjs.parentNode.insertBefore(js, fjs);
-   }(document, 'script', 'facebook-jssdk'));
-
-
-function checkLoginState() {
-    FB.getLoginStatus(function(response) {
-    statusChangeCallback(response);
+ 
+  function testAPI() {                      // Testing Graph API after login.  See statusChangeCallback() for when this call is made.
+    console.log('Welcome!  Fetching your information.... ');
+    FB.api('/me', function(response) {
+      console.log('Successful login for: ' + response.name);
+      document.getElementById('status').innerHTML =
+        'Thanks for logging in, ' + response.name + '!';
     });
-}
+  }
+
+</script>
+
+
+<!-- The JS SDK Login Button -->
+
+<fb:login-button scope="public_profile,email" onlogin="checkLoginState();">
+</fb:login-button>
+
+<div id="status">
+</div>
+
+<!-- Load the JS SDK asynchronously -->
+<script async defer crossorigin="anonymous" src="https://connect.facebook.net/en_US/sdk.js"></script>
+
+
+<script>
+//   window.fbAsyncInit = function() {
+//     FB.init({
+//       appId      : '451774609941079',
+//       cookie     : true,
+//       xfbml      : true,
+//       version    : 'v13.0'
+//     });
+      
+//     FB.AppEvents.logPageView();   
+      
+//   };
+
+//   (function(d, s, id){
+//      var js, fjs = d.getElementsByTagName(s)[0];
+//      if (d.getElementById(id)) {return;}
+//      js = d.createElement(s); js.id = id;
+//      js.src = "https://connect.facebook.net/en_US/sdk.js";
+//      fjs.parentNode.insertBefore(js, fjs);
+//    }(document, 'script', 'facebook-jssdk'));
+
+
+// function checkLoginState() {
+//     FB.getLoginStatus(function(response) {
+//     statusChangeCallback(response);
+//     });
+// }
 
 
 
@@ -128,20 +187,20 @@ function checkLoginState() {
 </script>
     <!-- Fim do Login Facebook -->
 
-    <fb:login-button 
-  scope="public_profile,email"
-  onlogin="checkLoginState();">
-</fb:login-button>
+    <!-- <fb:login-button  -->
+  <!-- scope="public_profile,email" -->
+  <!-- onlogin="checkLoginState();"> -->
+<!-- </fb:login-button> -->
 
 
     <!-- Page Preloder 
     <div id="preloder">
         <div class="loader"></div>
     </div> -->
-    <div id="fb-root"></div>
-<script async defer crossorigin="anonymous" src="https://connect.facebook.net/pt_BR/sdk.js#xfbml=1&version=v13.0&appId=451774609941079&autoLogAppEvents=1" nonce="gEcVnlTk"></script>
+    <!-- <div id="fb-root"></div> -->
+<!-- <script async defer crossorigin="anonymous" src="https://connect.facebook.net/pt_BR/sdk.js#xfbml=1&version=v13.0&appId=451774609941079&autoLogAppEvents=1" nonce="gEcVnlTk"></script> -->
 
-<div class="fb-login-button" data-width="" data-size="small" data-button-type="login_with" data-layout="default" data-auto-logout-link="true" data-use-continue-as="false"></div>
+<!-- <div class="fb-login-button" data-width="" data-size="small" data-button-type="login_with" data-layout="default" data-auto-logout-link="true" data-use-continue-as="false"></div> -->
 
     <header>
         <div class="card-frete">
